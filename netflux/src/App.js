9a19da6c9216786dfb4bbdd39aa8dd649e11d5ed
Athoug Alsoughayer <athoug2.s@gmail.com>
2019-10-27 08:26:47 +0300

    starter files

---
+++ netflux/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="container">
+          <h1>تطبيق Netflux</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
